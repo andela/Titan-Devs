464d882a358ee7df6381ef83f321182b584f3a58
YvesIraguha <iraguhaivos@gmail.com>
2019-02-06 21:33:09 +0200

    [ft #163756376] Create a login controller

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import userController from "../controllers/users";
+import Login from "../controllers/auth/login.controller";
 
 const userRouters = Router();
 
 userRouters.get("/users/:userId", userController.findOne);
+/** define login route */
+userRouters.post("/users/login", Login.authenticate);
 
 export default userRouters;
