43b00e32f0a3bd4391a98b686c02b970902faa5b
luc-abayo <luc.bayo@gmail.com>
2019-02-06 13:08:52 +0200

    [ch-integrate-coveralls-code-#163518680]Fix houndci errors

---
+++ tests/index.test.js
@@ -0,0 +1,17 @@
+const secret = require("../config/index").secret;
+const equal = require("chai").assert.equal;
+const addTwo = (a, b) => {
+  return parseFloat(a) + parseFloat(b);
+};
+
+describe("/addTwo", () => {
+  it("It should return sum of two number", done => {
+    const sum = addTwo(2, 4);
+    equal(sum, 6);
+    done();
+  });
+  it("It should return secret", done => {
+    equal(secret, "secret");
+    done();
+  });
+});
