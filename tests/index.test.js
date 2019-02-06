a7c68b3240e31150f37d765784e6748e46b26b00
luc-abayo <luc.bayo@gmail.com>
2019-02-06 11:14:54 +0200

    [ft-pwd-recovery-#163388780]Add more test sample

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,36 +1,21 @@
-<<<<<<< HEAD
 import { assert } from "chai";
 import config from "../config/index";
 
 const { equal } = assert;
-=======
-//sample test
-const secret = require("../config/index").secret;
 const assert = require("chai").assert;
->>>>>>> [ch-integrate-coveralls-code-#163518680]Add code coverage and coverage reporter
+const equal = require("chai").assert.equal;
 const addTwo = (a, b) => {
   return parseFloat(a) + parseFloat(b);
 };
-
-<<<<<<< HEAD
-describe("/addTwo ", () => {
+describe("/addTwo", () => {
   it("It should return sum of two number", done => {
     const sum = addTwo(2, 4);
     equal(sum, 6);
     done();
   });
   it("It should return secret", done => {
-    equal(config.secret, "secret");
-=======
-describe("/addTwo", () => {
-  it("It should return sum of two number", done => {
-    const sum = addTwo(2, 4);
-    assert.equal(sum, 6);
-    done();
-  });
-  it("It should return secret", done => {
     assert.equal(secret, "secret");
->>>>>>> [ch-integrate-coveralls-code-#163518680]Add code coverage and coverage reporter
+    equal(secret, "secret");
     done();
   });
 });
