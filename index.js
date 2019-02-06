9571fc2b820f0a5b472640b358384ac6140eec25
espoirMur <espoir.mur@gmail.com>
2019-02-06 11:51:08 +0200

    [ch #[163518673]] Comment method override middleware

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
-app.use(methodOverride());
+// app.use(methodOverride());
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
