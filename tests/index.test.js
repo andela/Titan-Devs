0957788f337b91acac03a5d5c42fce2e99dd370b
luc-abayo <luc.bayo@gmail.com>
2019-02-07 15:09:34 +0200

    #163756426 Set NODE_ENV=test before runing test

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,7 +6,7 @@ const addTwo = (a, b) => {
   return parseFloat(a) + parseFloat(b);
 };
 
-describe("/addTwo", () => {
+describe("/addTwo ", () => {
   it("It should return sum of two number", done => {
     const sum = addTwo(2, 4);
     equal(sum, 6);
