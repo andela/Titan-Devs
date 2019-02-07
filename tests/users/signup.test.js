68027cb7435b8458a29fce2db650a5d454cb049f
luc-abayo <luc.bayo@gmail.com>
2019-02-07 17:07:12 +0200

    #163756426 Adjust http status code

--- tests/users/signup.test.js
+++ tests/users/signup.test.js
@@ -16,7 +16,7 @@ describe("API end point for auth/signup ", () => {
       .request(app)
       .post("/api/v1/users")
       .send({ ...dammyUser });
-    expect(response.status).eql(200);
+    expect(response.status).eql(201);
     expect(response.body).to.be.an("object");
     expect(response.body).to.have.property("message");
     expect(response.body.message).to.be.equals("User registered successfully");
