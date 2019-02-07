dde58e7a7a0a06ab6697e74bfc3e860dfb4dc931
Musigwa Pacifique <pacifique.musigwa@gmail.com>
2019-02-07 12:35:20 +0200

    #163754751 Comment the application with JsDoc (#11)
    * [Chore #16375451] Comment the whole application with JsDoc
    
    * [Chore #163754751] Fix the ESLint plugin error

--- controllers/users.js
+++ controllers/users.js
@@ -3,20 +3,25 @@ import models from "../models";
 const { User } = models;
 
 class UserController {
+  /**
+   * Fetch a single user's information.
+   * @param  {Object} req - The request object.
+   * @param  {Object} res - The response object.
+   * @return {Object} The request response
+   */
   static findOne(req, res) {
     const { userId } = req.params;
     User.findOne({ where: { id: userId } })
       .then(result => {
         if (result) {
-          res.status(200).json({
+          return res.status(200).json({
             message: "User retrieved successfully",
             user: result.dataValues
           });
-        } else {
-          res.status(404).json({
-            message: "User not found "
-          });
         }
+        return res.status(404).json({
+          message: "User not found "
+        });
       })
       .catch(error => {
         res
