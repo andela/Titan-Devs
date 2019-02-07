045c259ebce2f8c4ed6379b6f4ed1bca9a84c0a7
luc-abayo <luc.bayo@gmail.com>
2019-02-07 09:30:18 +0200

    #163756426 Add email validation and test for email validator

--- migrations/seeders/20190130170205-create-user.js
+++ migrations/seeders/20190130170205-create-user.js
@@ -9,12 +9,9 @@ module.exports = {
           id: "dfe7fbc2-289d-11e9-b210-d663bd873d97",
           email: "test@ah.com",
           password: "password",
-<<<<<<< HEAD
           username: "luc",
-=======
           firstName: "User",
           lastName: "1",
->>>>>>> [ft-user-signup-#163756426]Add test for  and attributes validation
           createdAt: "2019-02-04T14:17:44.366Z",
           updatedAt: "2019-02-04T14:17:44.366Z"
         }
