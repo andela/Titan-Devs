a4e7af236fd5f365e4c1f11cc2f2e0cb2948f7b2
YvesIraguha <iraguhaivos@gmail.com>
2019-02-07 14:19:38 +0200

    [ft #163756376] Create validation for login credentials

--- controllers/auth/login.controller.js
+++ controllers/auth/login.controller.js
@@ -1,37 +1,33 @@
 import models from "../../models";
+import validate from "../../helpers/validators/login.validation";
 
 const { User } = models;
-
 /** Define class for handling user login */
 
 class Login {
-  static async authenticate(req, res) {
+  static authenticate(req, res) {
     const { email, password } = req.body;
-    const isEmpty = !email || !password ? "Provide valid credentials" : false;
-    /** if the email or password is empty, return an error message */
-    if (isEmpty) {
-      return res.status(400).json({ message: isEmpty });
+    /** Validate the email and password before any action */
+    const err = validate(email, password);
+    if (err) {
+      return res.status(400).json({ message: err.message });
     }
-    /** If password and email are not empty, use the email to search a user in database */
-    try {
-      const user = await User.findOne(
-        { attributes: ["email", "password", "id"] },
-        { where: { email } }
-      );
-      const error = !user ? "No user with that email" : false;
-      if (error) {
-        return res.status(404).json({ error });
-      }
-      if (user.password === password) {
-        /** Create a token for a user and send it */
-        return res
-          .status(200)
-          .send({ token: user, message: "Loged in successfully" });
-      }
-      return res.status(400).send({ message: "Wrong password, try again?" });
-    } catch (error) {
-      return res.status(500).json({ error: error.message });
-    }
+    /** Find if the user exists */
+    User.findOne({ where: { email }, attributes: ["email", "password", "id"] })
+      .then(result => {
+        if (!result) {
+          return res.status(404).json({ message: "Invalid email or password!" });
+        }
+        if (result.dataValues.password === password) {
+          return res
+            .status(200)
+            .send({ message: "Logged in successffully", token: result.dataValues });
+        }
+        return res.status(400).json({ message: "Invalid email or password!" });
+      })
+      .catch(error => {
+        return res.status(500).json({ error: error.message });
+      });
   }
 }
 
--- tests/users/login.test.js
+++ tests/users/login.test.js
@@ -16,7 +16,7 @@ describe("POSt /api/v1/users/login", () => {
   // create a script that delete the user after test suites.
   it("It should return a token", done => {
     const user = {
-      email: "test@test.com",
+      email: "test@ah.com",
       password: "password"
     };
     chai
@@ -25,8 +25,8 @@ describe("POSt /api/v1/users/login", () => {
       .send(user)
       .end((error, res) => {
         if (error) done(error.message);
-        res.body.should.have.property("message").eql("User logged in successfully");
-        res.body.should.have.property("token").which.is.a("string");
+        res.body.should.have.property("message").eql("Logged in successffully");
+        res.body.should.have.property("token").which.is.a("object");
         done();
       });
   });
@@ -42,7 +42,7 @@ describe("POSt /api/v1/users/login", () => {
       .end((error, res) => {
         if (error) done(error.message);
         res.body.should.have.property("message").eql("Please provide an email");
-        res.statusCode.should.be(400);
+        res.status.should.be.equal(400);
         done();
       });
   });
@@ -56,7 +56,7 @@ describe("POSt /api/v1/users/login", () => {
       .send(user)
       .end((error, res) => {
         if (error) done(error);
-        res.statusCode.should.be(400);
+        res.status.should.be.equal(400);
         res.body.should.have.property("message").eql("Please provide a password");
         done();
       });
@@ -72,7 +72,7 @@ describe("POSt /api/v1/users/login", () => {
       .send(user)
       .end((error, res) => {
         if (error) done(error.message);
-        res.statusCode.should.be(400);
+        res.status.should.be.equal(400);
         res.body.should.have.property("message").eql("Email cannot be empty");
         done();
       });
@@ -88,14 +88,14 @@ describe("POSt /api/v1/users/login", () => {
       .send(user)
       .end((error, res) => {
         if (error) done(error.message);
-        res.statusCode.should.be(400);
+        res.status.should.be.equal(400);
         res.body.should.have.property("message").eql("Password cannot be empty");
         done();
       });
   });
-  it("It should return a wrong password error", done => {
+  it("It should test a wrong password error", done => {
     const user = {
-      email: "test@test.com",
+      email: "test@ah.com",
       password: "passwor"
     };
     chai
@@ -104,14 +104,14 @@ describe("POSt /api/v1/users/login", () => {
       .send(user)
       .end((error, res) => {
         if (error) done(error.message);
-        res.statusCode.should.be(400);
-        res.body.should.have.property("message").eql("Wrong password, try again!");
+        res.status.should.be.equal(400);
+        res.body.should.have.property("message").eql("Invalid email or password!");
         done();
       });
   });
-  it("It should return a non-existing user error", done => {
+  it("It should test a non-existing user error", done => {
     const user = {
-      user: "user@user.com",
+      email: "user@user.com",
       password: "user@user"
     };
     chai
@@ -120,10 +120,24 @@ describe("POSt /api/v1/users/login", () => {
       .send(user)
       .end((error, res) => {
         if (error) done(error.message);
-        res.statusCode.should.be(400);
-        res.body.should.have
-          .property("message")
-          .eql("The user with those credentials does not exist");
+        res.status.should.be.equal(404);
+        res.body.should.have.property("message").eql("Invalid email or password!");
+        done();
+      });
+  });
+  it("It should test an email of wrong format", done => {
+    const user = {
+      email: "123.com",
+      password: "password"
+    };
+    chai
+      .request(app)
+      .post("/api/v1/users/login")
+      .send(user)
+      .end((error, res) => {
+        if (error) done(error.message);
+        res.status.should.be.equal(400);
+        res.body.should.have.property("message").eql("Invalid email format!");
         done();
       });
   });
