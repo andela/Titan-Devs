2e1628cc36436bd2a958f418eb455e4b29ea1659
luc-abayo <luc.bayo@gmail.com>
2019-02-07 19:28:22 +0200

    #163518684 Add joi schema for user attributes validation

--- middlewares/validators/signup.validator.js
+++ middlewares/validators/signup.validator.js
@@ -1,13 +1,29 @@
 import { isEmpty, isEmailValid } from "../../helpers/funcValidators";
 
 export default class SignupValidator {
-  static allAttributes(req, res, next) {
+  static async allAttributes(req, res, next) {
     const { email, password, username } = req.body;
     const errors = {};
     if (isEmpty(email)) errors.email = "Email is required";
     if (isEmpty(password)) errors.password = "Password is required";
     if (isEmpty(username)) errors.username = "Username is required";
-
+    // try {
+    //   const result = await joi.validate(req.body, userSchema);
+    //   console.log(result);
+    // } catch (error) {
+    //   const { message } = error.details[0];
+    //   switch (message) {
+    //     case JOI_ALPHANUMERIC_ERROR:
+    //       return (errors.password = "Password should be alphanumeric");
+    //     case JOI_INVALID_EMAIL_ERROR:
+    //       return (error.email = "Invalid email");
+    //     case JOI_SHORT_PASSWORD_ERROR:
+    //       return (errors.password = "Password should be at least 8");
+    //     default:
+    //       return;
+    //   }
+    //   console.log(message);
+    // }
     return isEmpty(errors)
       ? next()
       : res.status(400).json({ message: "User registration failed", errors });
