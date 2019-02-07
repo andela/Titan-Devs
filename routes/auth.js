922316c9700798113a448968da92318fafe31507
luc-abayo <luc.bayo@gmail.com>
2019-02-07 09:30:18 +0200

    #163756426 Add email validation and test for email validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import AuthController from "../controllers/auth/authController";
-import SignupValidation from "../middlewares/validation/signup.validator";
+import SignupValidation from "../middlewares/validators/signup.validator";
 
 const authRouters = Router();
 
 authRouters.post(
   "/auth/signup",
   SignupValidation.allAttributes,
+  SignupValidation.validateEmail,
   AuthController.signUp
 );
 
