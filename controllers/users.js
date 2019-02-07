170b4f6d807dadd337e607e1fa464ef33781565b
luc-abayo <luc.bayo@gmail.com>
2019-02-07 15:42:46 +0200

    #163756426 Add console.log for debugging

--- controllers/users.js
+++ controllers/users.js
@@ -39,7 +39,6 @@ class UserController {
         }
       });
     } catch (error) {
-      console.log(error);
       if (error.name === "SequelizeUniqueConstraintError") {
         const { message } = error.errors[0];
         let errorMessage = message;
@@ -49,7 +48,7 @@ class UserController {
           errorMessage = "The username is already taken";
         return res.status(409).json({ message: errorMessage });
       }
-      return res.status(500).json({
+      res.status(500).json({
         message: "User registration failed, try again later!",
         errors: error.stack.Error
       });
