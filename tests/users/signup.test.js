acd51623642964c0c3bdff492cdb82683bbe9653
luc-abayo <luc.bayo@gmail.com>
2019-02-07 17:00:33 +0200

    #163756426 Change error messages

--- tests/users/signup.test.js
+++ tests/users/signup.test.js
@@ -15,7 +15,7 @@ describe("API end point for auth/signup ", () => {
     const response = await chai
       .request(app)
       .post("/api/v1/users")
-      .send({ ...userDamie });
+      .send({ ...dammyUser });
     expect(response.status).eql(200);
     expect(response.body).to.be.an("object");
     expect(response.body).to.have.property("message");
