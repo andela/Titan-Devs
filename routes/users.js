19a093f675bbd985bee0fb9b9c32601f62e46a0a
luc-abayo <luc.bayo@gmail.com>
2019-02-08 11:15:53 +0200

    #163518684 Add email, username, and password validation

--- routes/users.js
+++ routes/users.js
@@ -9,6 +9,8 @@ userRouters.post(
   "/users",
   SignupValidation.allAttributes,
   SignupValidation.validateEmail,
+  SignupValidation.validatePassword,
+  SignupValidation.validateUsername,
   UserController.signUp
 );
 export default userRouters;
