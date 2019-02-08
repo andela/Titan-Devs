790af10da764ccb36dbc5164188be18cfae03ab7
luc-abayo <luc.bayo@gmail.com>
2019-02-08 11:28:48 +0200

    #163518684 Remove yarn.lock duplicate

--- tests/index.test.js
+++ tests/index.test.js
@@ -7,7 +7,7 @@ const equal = require("chai").assert.equal;
 const addTwo = (a, b) => {
   return parseFloat(a) + parseFloat(b);
 };
-describe("/addTwo ", () => {
+describe("/addTwo", () => {
   it("It should return sum of two number", done => {
     const sum = addTwo(2, 4);
     equal(sum, 6);
