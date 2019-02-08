a0e74e98343d8a8d7acaa5f713edb419da154215
luc-abayo <luc.bayo@gmail.com>
2019-02-08 11:15:53 +0200

    #163518684 Add email, username, and password validation

--- helpers/funcValidators.js
+++ helpers/funcValidators.js
@@ -6,7 +6,6 @@ export const isEmpty = value => {
     (typeof value === "string" && !value.trim().length)
   );
 };
-
 export const isEmailValid = email => {
   const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
   return emailRegex.test(email);
