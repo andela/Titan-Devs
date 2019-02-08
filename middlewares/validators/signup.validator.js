f4ef50242551e988067a6adc62dbfdf49d3c3f29
luc-abayo <luc.bayo@gmail.com>
2019-02-08 11:15:53 +0200

    #163518684 Add email, username, and password validation

--- middlewares/validators/signup.validator.js
+++ middlewares/validators/signup.validator.js
@@ -1,11 +1,4 @@
-import joi from "joi";
-import { isEmpty, isEmailValid } from "../../helpers/funcValidators";
-import { userSchema } from "../../helpers/db.schema";
-import {
-  JOI_ALPHANUMERIC_ERROR,
-  JOI_INVALID_EMAIL_ERROR,
-  JOI_SHORT_PASSWORD_ERROR
-} from "../../helpers/constants";
+import { isEmpty, isEmailValid, isAlphanumeric } from "../../helpers/funcValidators";
 
 export default class SignupValidator {
   static async allAttributes(req, res, next) {
@@ -14,23 +7,6 @@ export default class SignupValidator {
     if (isEmpty(email)) errors.email = "Email is required";
     if (isEmpty(password)) errors.password = "Password is required";
     if (isEmpty(username)) errors.username = "Username is required";
-    // try {
-    //   const result = await joi.validate(req.body, userSchema);
-    //   console.log(result);
-    // } catch (error) {
-    //   const { message } = error.details[0];
-    //   switch (message) {
-    //     case JOI_ALPHANUMERIC_ERROR:
-    //       return (errors.password = "Password should be alphanumeric");
-    //     case JOI_INVALID_EMAIL_ERROR:
-    //       return (error.email = "Invalid email");
-    //     case JOI_SHORT_PASSWORD_ERROR:
-    //       return (errors.password = "Password should be at least 8");
-    //     default:
-    //       return;
-    //   }
-    //   console.log(message);
-    // }
     return isEmpty(errors)
       ? next()
       : res.status(400).json({ message: "User registration failed", errors });
@@ -41,5 +17,26 @@ export default class SignupValidator {
     return isEmailValid(email)
       ? next()
       : res.status(400).json({ message: "Invalid email" });
+  }
+
+  static validatePassword(req, res, next) {
+    const { password } = req.body;
+    const isPassword = isAlphanumeric(password) && password.trim().length >= 8;
+    return isPassword
+      ? next()
+      : res.status(400).json({
+          message:
+            "The password should be an alphanumeric with at least 8 characters"
+        });
+  }
+
+  static validateUsername(req, res, next) {
+    const { username } = req.body;
+    return isAlphanumeric(username) && /[a-z]/i.test(username.trim()[0])
+      ? next()
+      : res.status(400).json({
+          message:
+            "The username must begin with letter and only contains alphabet and numbers not symbols"
+        });
   }
 }
