bdcd2ddab87c84a2e64a590a0f517cef99875527
luc-abayo <luc.bayo@gmail.com>
2019-02-11 18:30:23 +0200

    #163518685 Add password reset email tamplate

--- helpers/resetPasswordTamplate.js
+++ helpers/resetPasswordTamplate.js
@@ -21,7 +21,11 @@ const resetPasswordTamplate = token => {
                 >
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 Author's Heaven
+=======
+                Titan Devs
+>>>>>>> #163518685 Add password reset email tamplate
 =======
                 Titan Devs
 >>>>>>> #163518685 Add password reset email tamplate
@@ -48,6 +52,7 @@ const resetPasswordTamplate = token => {
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                     href=${process.env.SERVER_HOST}/users/${token}/password
 =======
                     href="#"
@@ -61,6 +66,9 @@ const resetPasswordTamplate = token => {
 =======
                     href=${process.env.SERVER_HOST}/users/${token}/password
 >>>>>>> #163518685 Add mailer for password rest
+=======
+                    href="#"
+>>>>>>> #163518685 Add password reset email tamplate
                     target="_blank"
                     style="font-size: 20px; font-family: Helvetica, Arial, sans-serif; color: #ffffff; text-decoration: none; color: #ffffff; text-decoration: none; padding: 15px 25px; border-radius: 2px; border: 1px solid rgb(0,47,255); display: inline-block; text-align: center"
                     >
