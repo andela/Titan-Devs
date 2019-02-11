7ef0b34ff6f0b4f4c2de280b7748c4b264034569
luc-abayo <luc.bayo@gmail.com>
2019-02-11 18:30:23 +0200

    #163518685 Add password reset email tamplate

---
+++ services/sendgrid.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv";
+import emailTamplate from "../helpers/resetPasswordTamplate";
+import sgMail from "@sendgrid/mail";
+
+dotenv.config();
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export const sendEmail = async (email, token) => {
+  const message = {
+    to: email,
+    from: { email: process.env.DEVELOPER_EMAIL, name: "Titan Devs" },
+    subject: `Author's Heaven Password Reset`,
+    html: emailTamplate(token)
+  };
+
+  const response = await sgMail.send(message);
+  return response;
+};
