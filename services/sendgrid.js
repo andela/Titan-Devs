5c931d57356c68aa28403c56a83db151e3fa3054
luc-abayo <luc.bayo@gmail.com>
2019-02-11 20:02:37 +0200

    #163518685 Add mailer for password rest

--- services/sendgrid.js
+++ services/sendgrid.js
@@ -1,28 +1,15 @@
 import dotenv from "dotenv";
-<<<<<<< HEAD
-=======
-import emailTamplate from "../helpers/resetPasswordTamplate";
->>>>>>> #163518685 Add password reset email tamplate
 import sgMail from "@sendgrid/mail";
 
 dotenv.config();
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-<<<<<<< HEAD
 export const sendEmail = async (email, subject, emailTamplate) => {
   const message = {
     to: email,
     from: { email: process.env.DEVELOPER_EMAIL, name: "Author's Heaven" },
     subject: subject,
     html: emailTamplate
-=======
-export const sendEmail = async (email, token) => {
-  const message = {
-    to: email,
-    from: { email: process.env.DEVELOPER_EMAIL, name: "Titan Devs" },
-    subject: `Author's Heaven Password Reset`,
-    html: emailTamplate(token)
->>>>>>> #163518685 Add password reset email tamplate
   };
 
   const response = await sgMail.send(message);
