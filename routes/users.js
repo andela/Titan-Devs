bc24bd203b573013e9080593b9449097e4f6d451
luc-abayo <luc.bayo@gmail.com>
2019-02-11 15:39:56 +0200

    #163518685 Add reset and update password tests

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,16 @@ import SignupValidation from "../middlewares/validators/signup.validator";
 
 const userRouters = Router();
 
-userRouters.post(
-  "/users",
-  SignupValidation.allAttributes,
-  SignupValidation.validateEmail,
-  SignupValidation.validatePassword,
-  SignupValidation.validateUsername,
-  UserController.signUp
-);
-
 userRouters.post("/users/login", Login.signIn);
+userRouters
+  .post(
+    "/users",
+    SignupValidation.allAttributes,
+    SignupValidation.validateEmail,
+    SignupValidation.validatePassword,
+    SignupValidation.validateUsername,
+    UserController.signUp
+  )
+  .post("/users/reset_password", UserController.resetPassword)
+  .put("/users/:token/password", UserController.updatePassword);
 export default userRouters;
