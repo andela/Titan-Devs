2fcd079b6f38ca0614aa234abc0ab7dcd7ba5fde
luc-abayo <luc.bayo@gmail.com>
2019-02-12 11:43:02 +0200

    #163518685 Make password reset token one time usable

--- controllers/users.js
+++ controllers/users.js
@@ -2,14 +2,7 @@ import dotenv from "dotenv";
 import { hashSync, genSaltSync } from "bcrypt";
 import jwt from "jsonwebtoken";
 import models from "../models";
-<<<<<<< HEAD
-<<<<<<< HEAD
 import resetPwdTamplage from "../helpers/resetPasswordTamplate";
-=======
->>>>>>> #163518685 Add password reset email tamplate
-=======
-import resetPwdTamplage from "../helpers/resetPasswordTamplate";
->>>>>>> #163518685 Add mailer for password rest
 import { sendEmail } from "../services/sendgrid";
 
 dotenv.config();
@@ -54,7 +47,6 @@ class UserController {
   }
 
   static async resetPassword(req, res) {
-<<<<<<< HEAD
     if (!req.body.email) {
       return res.status(400).json({ message: "Email is required" });
     }
@@ -87,33 +79,6 @@ class UserController {
         }
       });
     } catch (error) {
-=======
-    const { email } = req.body;
-    if (!email) {
-      return res.status(400).json({ message: "Email is required" });
-    }
-    try {
-      const user = await User.findOne({
-        where: {
-          email
-        }
-      });
-      if (!user) {
-        return res.status(404).json({ message: "User not found" });
-      }
-      const token = await jwt.sign(email, process.env.SECRET_OR_KEY);
-      const emailBody = await resetPwdTamplage(token);
-      const emailResponse = await sendEmail(email, "Password Reset", emailBody);
-      if (emailResponse.length > 0 && emailResponse[0].statusCode === 202) {
-        res.json({ message: "Password rest link sent", token });
-      } else {
-        res
-          .status(500)
-          .json({ message: "Error while sending email", data: emailResponse });
-      }
-    } catch (error) {
-      console.log(error);
->>>>>>> #163518685 Add mailer for password rest
       res.status(500).json({ message: "Unknown error occurred" });
     }
   }
@@ -123,19 +88,12 @@ class UserController {
     try {
       await jwt.verify(token, process.env.SECRET_OR_KEY, async (error, email) => {
         if (error) {
-<<<<<<< HEAD
           return res.status(400).json({ message: "Invalid or expired link" });
-=======
-          return res
-            .status(400)
-            .json({ message: "Invalid or experied token provided" });
->>>>>>> #163518685 Add mailer for password rest
         }
         const salt = await genSaltSync(
           parseFloat(process.env.BCRYPT_HASH_ROUNDS) || 10
         );
         const password = await hashSync(req.body.password, salt);
-<<<<<<< HEAD
         User.findOne({
           where: {
             email,
@@ -148,20 +106,6 @@ class UserController {
           await response.update({ password, resetToken: null });
           return res.json({ message: "Password updated" });
         });
-=======
-        const user = await User.update(
-          { password },
-          {
-            where: {
-              email
-            }
-          }
-        );
-        if (!user) {
-          return res.status(404).json({ message: "User not found" });
-        }
-        return res.json({ message: "Password updated" });
->>>>>>> #163518685 Add mailer for password rest
       });
     } catch (error) {
       res
