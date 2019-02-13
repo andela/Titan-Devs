008adfa353ea1afbef05f7cacaac71f11c8c28e9
YvesIraguha <iraguhaivos@gmail.com>
2019-02-13 18:49:26 +0200

    [ft #163518695] include the passport configurations

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ import morgan from "morgan";
 import swaggerUi from "swagger-ui-express";
 import routers from "./routes";
 import swaggerDocument from "./swagger.json";
+import passport from "passport";
+import passportConfig from "./middlewares/passport";
 
 dotenv.config();
 const isProduction = process.env.NODE_ENV === "production";
@@ -25,6 +27,8 @@ app.use(bodyParser.json());
 app.use(morgan("dev"));
 
 app.use(express.static(`${__dirname}/public`));
+app.use(passport.initialize());
+passportConfig(passport);
 
 app.use(
   session({
@@ -42,44 +46,37 @@ if (!isProduction) {
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1/", routers);
 
-// catch 404 and forward to error handler
-app.use(next => {
-  const err = new Error("Not Found");
-  err.status = 404;
-  next(err);
-});
-
 // error handlers
 
 // development error handler
 // will print stacktrace
-if (!isProduction) {
-  app.use((err, res) => {
-    // eslint-disable-next-line no-console
-    console.log(err.stack);
+// if (!isProduction) {
+//   app.use((err, res) => {
+//     // eslint-disable-next-line no-console
+//     console.log(err.stack);
 
-    res.status(err.status || 500);
+//     res.status(err.status || 500);
 
-    res.json({
-      errors: {
-        message: err.message,
-        error: err
-      }
-    });
-  });
-}
+//     res.json({
+//       errors: {
+//         message: err.message,
+//         error: err
+//       }
+//     });
+//   });
+// }
 
 // production error handler
 // no stacktraces leaked to user
-app.use((err, res) => {
-  res.status(err.status || 500);
-  res.json({
-    errors: {
-      message: err.message,
-      error: {}
-    }
-  });
-});
+// app.use((err, res) => {
+//   res.status(err.status || 500);
+//   res.json({
+//     errors: {
+//       message: err.message,
+//       error: {}
+//     }
+//   });
+// });
 
 // finally, let's start our server...
 const server = app.listen(process.env.PORT || 3000, () => {
