3f4db0ce233a1c5038281c88483fea1b97c89f07
Jean Luc Abayo <luc.bayo@gmail.com>
2019-02-14 02:50:31 -0800

    163518685 Password Reset (#15)
    * #163756426 Add email validation and test for email validator
    
    * #163756426 Add user registration endpoint
    
    * #163756426 Resolve houndci warning
    
    * #163756426 improve test script in package json
    
    * #163518684 Add joi schema for user attributes validation
    
    * #163518684 Add email, username, and password validation
    
    * #163518685 Change SECRET_KEY to SECRET_OR_KEY
    
    * #163518685 Prevent to update password from the previous one
    
    * #163518685 Add token model
    
    * #163518685 Add userToken model
    
    * #163518685 Prevent user to reset password from the previos one
    
    * #163518685 Remove consoles for dubugging
    
    * Change password reset email notification

---
+++ helpers/data.js
@@ -0,0 +1,12 @@
+export const data = {
+  dummyUser: {
+    email: "luc.bayo@gmail.com",
+    password: "password",
+    username: "luc2017"
+  },
+  dummyUser2: {
+    email: "fake.email@gmail.com",
+    password: "password",
+    username: "jean786786"
+  }
+};
---
+++ migrations/20190212084133-add-reset-token-to-users.js
@@ -0,0 +1,30 @@
+"use strict";
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return Promise.all([
+      queryInterface.addColumn("users", "resetToken", {
+        type: Sequelize.TEXT,
+        defaultValue: null
+      })
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return Promise.all([queryInterface.removeColumn("users", "resetToken")]);
+  }
+};
