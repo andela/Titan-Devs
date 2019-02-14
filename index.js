eee47aa18fc1ea80466c59742005c80912a253c6
YvesIraguha <iraguhaivos@gmail.com>
2019-02-14 09:55:32 +0200

    [ft #163518695] include the passport configurations

--- index.js
+++ index.js
@@ -46,37 +46,44 @@ if (!isProduction) {
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/v1/", routers);
 
+// catch 404 and forward to error handler
+app.use(next => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
 // error handlers
 
 // development error handler
 // will print stacktrace
-// if (!isProduction) {
-//   app.use((err, res) => {
-//     // eslint-disable-next-line no-console
-//     console.log(err.stack);
+if (!isProduction) {
+  app.use((err, res) => {
+    // eslint-disable-next-line no-console
+    console.log(err.stack);
 
-//     res.status(err.status || 500);
+    res.status(err.status || 500);
 
-//     res.json({
-//       errors: {
-//         message: err.message,
-//         error: err
-//       }
-//     });
-//   });
-// }
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
 
 // production error handler
 // no stacktraces leaked to user
-// app.use((err, res) => {
-//   res.status(err.status || 500);
-//   res.json({
-//     errors: {
-//       message: err.message,
-//       error: {}
-//     }
-//   });
-// });
+app.use((err, res) => {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
+});
 
 // finally, let's start our server...
 const server = app.listen(process.env.PORT || 3000, () => {
