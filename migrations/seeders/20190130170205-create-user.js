8dc7862484306d56b026637f0782edf24437bc6a
Fabrice NIYOMWUNGERI <niyomwungeri@Fabrices-MacBook-Pro.local>
2019-02-14 18:06:20 +0200

    [#163518700] remove conflit

--- migrations/seeders/20190130170205-create-user.js
+++ migrations/seeders/20190130170205-create-user.js
@@ -9,28 +9,9 @@ module.exports = {
           id: "dfe7fbc2-289d-11e9-b210-d663bd873d97",
           email: "test@ah.com",
           password: "password",
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> [ft-user-signup-#163756426]Add test for  and attributes validation
-=======
->>>>>>> #163756426 Add eslint-plugin-jsdoc@latest
           username: "luc",
           firstName: "User",
-          lastName: "1",
-<<<<<<< HEAD
->>>>>>> [ft-user-signup-#163756426]Add test for  and attributes validation
-<<<<<<< HEAD
-=======
-          username: "luc",
->>>>>>> #163756426 Add eslint-plugin-jsdoc@latest
-=======
->>>>>>> [ft-user-signup-#163756426]Add test for  and attributes validation
-=======
-          username: "luc",
->>>>>>> #163756426 Add eslint-plugin-jsdoc@latest
+          lastName: "1",:
           createdAt: "2019-02-04T14:17:44.366Z",
           updatedAt: "2019-02-04T14:17:44.366Z"
         }
