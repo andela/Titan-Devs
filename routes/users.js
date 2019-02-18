54e4633507aebbd8801b0484999e567e08eac6d3
Jean Luc Abayo <luc.bayo@gmail.com>
2019-02-18 10:22:07 +0200

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -22,5 +22,4 @@ userRouters
     SignupValidation.validatePassword,
     UserController.updatePassword
   );
-// userRouters.get("/users/:userId", UserController.findOne);
 export default userRouters;
