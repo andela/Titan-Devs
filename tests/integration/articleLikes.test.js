16907c948a9e06f188edfc060c361714205eae5d
luc-abayo <luc.bayo@gmail.com>
2019-02-20 18:45:37 +0200

    [#163518676]Removed eslint-plugin-jsdoc from eslint config

--- tests/integration/articleLikes.test.js
+++ tests/integration/articleLikes.test.js
@@ -30,10 +30,6 @@ describe("ArticleLike Controller", () => {
       article: { slug }
     } = articleResult.body);
   });
-<<<<<<< HEAD
-
-=======
->>>>>>> [#163518692]Enable user to like or dislike an article
   describe("article fetch and like", () => {
     describe("Like", () => {
       it("should allow  user to like an article", done => {
@@ -52,12 +48,8 @@ describe("ArticleLike Controller", () => {
             done();
           });
       });
-<<<<<<< HEAD
 
       it("should remove like when user try to like article twice", done => {
-=======
-      it("should deslike article when user try to like article twice", done => {
->>>>>>> [#163518692]Enable user to like or dislike an article
         chai
           .request(app)
           .post(`/api/v1/articles/${slug}/likes`)
@@ -66,7 +58,6 @@ describe("ArticleLike Controller", () => {
             if (err) {
               return done(err);
             }
-<<<<<<< HEAD
             expect(response.status).eql(200);
             expect(response.body).to.an("object");
             expect(response.body).to.have.property("message");
@@ -75,15 +66,6 @@ describe("ArticleLike Controller", () => {
           });
       });
 
-=======
-            expect(response.status).eql(202);
-            expect(response.body).to.an("object");
-            expect(response.body).to.have.property("message");
-            expect(response.body.message).to.be.eql("Successfully disliked");
-            done();
-          });
-      });
->>>>>>> [#163518692]Enable user to like or dislike an article
       it("should fail on non existing slug", done => {
         chai
           .request(app)
@@ -103,10 +85,6 @@ describe("ArticleLike Controller", () => {
             done();
           });
       });
-<<<<<<< HEAD
-
-=======
->>>>>>> [#163518692]Enable user to like or dislike an article
       it("should if user is not authenticated", done => {
         chai
           .request(app)
@@ -123,17 +101,10 @@ describe("ArticleLike Controller", () => {
     });
 
     describe("Dislike", () => {
-<<<<<<< HEAD
       it("should allow  user to dislike an article", done => {
         chai
           .request(app)
           .post(`/api/v1/articles/${slug}/dislikes`)
-=======
-      it("should allow  user to like an article", done => {
-        chai
-          .request(app)
-          .post(`/api/v1/articles/${slug}/likes`)
->>>>>>> [#163518692]Enable user to like or dislike an article
           .set({ Authorization: `Bearer ${token}` })
           .end((err, response) => {
             if (err) {
@@ -142,7 +113,6 @@ describe("ArticleLike Controller", () => {
             expect(response.status).eql(201);
             expect(response.body).to.an("object");
             expect(response.body).to.have.property("message");
-<<<<<<< HEAD
             expect(response.body.message).to.be.eql("Successfully disliked");
             done();
           });
@@ -152,22 +122,11 @@ describe("ArticleLike Controller", () => {
         chai
           .request(app)
           .post(`/api/v1/articles/${slug}/dislikes`)
-=======
-            expect(response.body.message).to.be.eql("Successfully liked");
-            done();
-          });
-      });
-      it("should allow  user to deslike an article", done => {
-        chai
-          .request(app)
-          .delete(`/api/v1/articles/${slug}/likes`)
->>>>>>> [#163518692]Enable user to like or dislike an article
           .set({ Authorization: `Bearer ${token}` })
           .end((err, response) => {
             if (err) {
               return done(err);
             }
-<<<<<<< HEAD
             expect(response.status).eql(200);
             expect(response.body).to.an("object");
             expect(response.body).to.have.property("message");
@@ -180,19 +139,6 @@ describe("ArticleLike Controller", () => {
         chai
           .request(app)
           .post(`/api/v1/articles/${slug}i97/dislikes`)
-=======
-            expect(response.status).eql(202);
-            expect(response.body).to.an("object");
-            expect(response.body).to.have.property("message");
-            expect(response.body.message).to.be.eql("Successfully disliked");
-            done();
-          });
-      });
-      it("should fail on non existing slug", done => {
-        chai
-          .request(app)
-          .delete(`/api/v1/articles/${slug}i97/likes`)
->>>>>>> [#163518692]Enable user to like or dislike an article
           .set({
             Authorization: `Bearer ${token}`
           })
@@ -208,18 +154,11 @@ describe("ArticleLike Controller", () => {
             done();
           });
       });
-<<<<<<< HEAD
 
       it("should if user is not authenticated", done => {
         chai
           .request(app)
           .post(`/api/v1/articles/${slug}/dislikes`)
-=======
-      it("should if user is not authenticated", done => {
-        chai
-          .request(app)
-          .delete(`/api/v1/articles/${slug}/likes`)
->>>>>>> [#163518692]Enable user to like or dislike an article
           .set({ Authorization: `Bear ${token}yi` })
           .end((err, response) => {
             if (err) {
@@ -235,11 +174,7 @@ describe("ArticleLike Controller", () => {
       it("should retrieve article with likes", done => {
         chai
           .request(app)
-<<<<<<< HEAD
           .get(`/api/v1/articles/${slug}/likes`)
-=======
-          .get(`/api/v1/articles/${slug}`)
->>>>>>> [#163518692]Enable user to like or dislike an article
           .set({ Authorization: `Bearer ${token}` })
           .end((err, response) => {
             if (err) {
@@ -258,18 +193,11 @@ describe("ArticleLike Controller", () => {
             done();
           });
       });
-<<<<<<< HEAD
 
       it("should return not found on non existing slug", done => {
         chai
           .request(app)
           .get(`/api/v1/articles/${slug}i97/likes`)
-=======
-      it("should return not found on non existing slug", done => {
-        chai
-          .request(app)
-          .get(`/api/v1/articles/${slug}i97`)
->>>>>>> [#163518692]Enable user to like or dislike an article
           .set({
             Authorization: `Bearer ${token}`
           })
@@ -284,19 +212,11 @@ describe("ArticleLike Controller", () => {
             done();
           });
       });
-<<<<<<< HEAD
-
-=======
->>>>>>> [#163518692]Enable user to like or dislike an article
       it("should if user is not authenticated", done => {
         chai
           .request(app)
           .get(`/api/v1/articles/${slug}`)
-<<<<<<< HEAD
           .set({ Authorization: `Bear ${token}yi/likes` })
-=======
-          .set({ Authorization: `Bear ${token}yi` })
->>>>>>> [#163518692]Enable user to like or dislike an article
           .end((err, response) => {
             if (err) {
               return done(err);
