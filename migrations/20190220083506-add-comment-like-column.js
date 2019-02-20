d2bd15a97f8b261086751bf4986226f998b4c3bc
YvesIraguha <iraguhaivos@gmail.com>
2019-02-20 13:02:45 +0200

    [feature #163518703] remove comments from models creation

--- migrations/20190220083506-add-comment-like-column.js
+++ migrations/20190220083506-add-comment-like-column.js
@@ -2,13 +2,6 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
     return Promise.all([
       queryInterface.addColumn("comments", "like", {
         type: Sequelize.INTEGER,
@@ -19,13 +12,6 @@ module.exports = {
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
     return Promise.all([queryInterface.removeColumn("comments", "like")]);
   }
 };
