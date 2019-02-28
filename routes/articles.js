6f7cd681bb7ee3a9e34665aedd131b5fa7e0ce8a
Fabrice NIYOMWUNGERI <niyomwungeri@Fabrices-MacBook-Pro.local>
2019-02-28 11:16:42 +0200

    [#164238813] remove share feature

--- routes/articles.js
+++ routes/articles.js
@@ -4,7 +4,6 @@ import articleValidator from "../middlewares/articleValidator";
 import ArticleLikeController from "../controllers/articleLikesController";
 import BookmarkController from "../controllers/bookmarkController";
 import ReportArticleController from "../controllers/reportArticleController";
-import ShareController from '../controllers/shareController';
 import checkAuth from "../middlewares/checkAuth";
 
 const article = Router();
@@ -16,25 +15,6 @@ article.get("/articles/:slug/likes", ArticleLikeController.getArticleLikes);
 
 article
   .post("/articles", checkAuth, Article.create)
-  .get(
-    "/articles/:slug/share/twitter",
-    checkAuth,
-    articleValidator.validateArticle,
-    ShareController.shareOnTwitter
-  )
-  .get(
-    "/articles/:slug/share/fb",
-    checkAuth,
-    articleValidator.validateArticle,
-    ShareController.shareOnFacebook
-  )
-  .get(
-    "/articles/:slug/share/linkedIn",
-    checkAuth,
-    articleValidator.validateArticle,
-    ShareController.shareOnLinkedin
-  )
-  .get("/articles/:slug/share/email", checkAuth, ShareController.shareOnEmail)
   .get("/articles/:slug", Article.findOneArticle);
 article.put(
   "/articles/:slug/report",
