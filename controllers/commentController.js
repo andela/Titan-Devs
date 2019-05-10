9f5ac9a37f64f8020c5a511995408f593d73fe18
Fabrice NIYOMWUNGERI <niomwungeri.fabrice@gmail.com>
2019-05-10 08:48:37 +0200

    fix comments

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -195,13 +195,14 @@ export default class CommentController {
   static async fetchAllComments(req, res) {
     try {
       const { slug } = req.params;
-      const { page } = req.query;
+      const { pageNumber = 1, limit = 10 } = req.query;
       const article = await Article.findOne({
         where: { slug },
         include: [
           {
             model: Comment,
             as: "comments",
+            order: [["createdAt", "DESC"]],
             attributes: [
               "articleId",
               "body",
@@ -210,8 +211,8 @@ export default class CommentController {
               "updatedAt",
               "like"
             ],
-            offset: page,
-            limit: 20,
+            offset: (Number(pageNumber) - 1) * Number(limit),
+            limit: Number(limit),
             include: [
               {
                 model: User,
@@ -232,7 +233,7 @@ export default class CommentController {
           .status(NOT_FOUND)
           .json({ message: "There is no article with that slug" });
       }
-      res.status(OK).json({ article });
+      res.status(OK).json({ article, commentsLength: article.comments.length });
     } catch (error) {
       res.status(INTERNAL_SERVER_ERROR).json({ message: error.message });
     }
